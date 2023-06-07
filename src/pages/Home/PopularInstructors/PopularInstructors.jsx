b712b5524c31a47bc4f41b5f7bfae94cefd187c0
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-06-08 00:41:48 +0600

    popular instructors design added

--- src/pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PopularInstructors = () => {
+    const [instructors, setInstructors] = useState([])
+    useEffect(() => {
+        fetch('instructors.json')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.name)
+                setInstructors(data)
+            })
+    }, [])
     return (
         <div>
-        PopularInstructors
-        </div>
+        {
+            instructors.map(instructor => 
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src={instructor.image} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{instructor.name}</h2>
+                        <p>{instructor.email}</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            )
+        }</div>
     );
 };
 
