b3c6e2832806885164ea00aabb1eedec5433f54c
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-06-07 16:48:06 +0600

    signup page added

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,11 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Signup = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePassword = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
+    const onSubmit = (data) => {
+        console.log(data.name, data.email, data.password, data.photo)
+    };
+
     return (
-        <div>
-        Signup
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className='w-full ml-5 mr-5 md:w-1/4 md:mx-auto mb-20'>
+
+            {/*Name */}
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Name</span>
+                </label>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                    className="input input-bordered"
+                    {...register("name", { required: true, maxLength: 20 })}
+                />
+                {errors.name && <span>This field is required</span>}
+            </div>
+
+            {/*Email */}
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    {...register("email")}
+                />
+            </div>
+
+
+            {/*photo url */}
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">PhotoUrl</span>
+                </label>
+                <input
+                    type="url"
+                    name="photo"
+                    placeholder="photo"
+                    className="input input-bordered"
+                    {...register("photo")}
+                />
+            </div>
+
+            {/*password */}
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Password</span>
+                </label>
+                <input placeholder='Give your Password' className="input input-bordered"
+                    type={passwordVisible ? 'text' : 'password'}
+                    {...register("password", {
+                        required: true,
+                        minLength: 6,
+                        pattern: /^(?=.*[A-Z])(?=.*[!@#$%^&*])/,
+                    })}
+                />
+                {errors.password?.type === 'required' && <span>This field is required</span>}
+                {errors.password?.type === 'minLength' && (
+                    <span>Password must be at least 6 characters long</span>
+                )}
+                {errors.password?.type === 'pattern' && (
+                    <span>Password must contain a capital letter and a special character</span>
+                )}
+
+                <button type="button" className='btn btn-primary mt-5 w-2/3' onClick={togglePassword}>
+                    {passwordVisible ? 'Hide' : 'Show'}
+                </button>
+            </div>
+
+
+
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Password</span>
+                </label>
+                <input placeholder='Confirm your Password' className="input input-bordered"
+                    type={passwordVisible ? 'text' : 'password'}
+                    {...register("confirmPassword", {
+                        required: true,
+                        validate: (value) => value === watch("password"),
+                    })}
+                />
+                {errors.confirmPassword && (
+                    <span>Passwords do not match</span>
+                )}
+            </div>
+
+
+            <input type="submit" className='btn btn-primary mt-5 w-2/3' value="Submit" />
+        </form>
     );
+
 };
 
 export default Signup;
